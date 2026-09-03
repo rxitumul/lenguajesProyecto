ccf7394e664bbf9b0d4b2b9f435095871edd1311
rxitumul <richi22castillox@gmail.com>
2026-09-03 17:22:07 -0600

    creacion

---
+++ src/main/java/com/mycompany/lenguajesproyecto/LenguajesProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lenguajesproyecto;
+
+/**
+ *
+ * @author ricardocastillo
+ */
+public class LenguajesProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
