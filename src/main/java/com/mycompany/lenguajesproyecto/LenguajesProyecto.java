fe8199f8821fc72777b6ff720f3f696ac9e61090
rxitumul <richi22castillox@gmail.com>
2026-09-14 11:37:21 -0600

    actualizacion de interfaces grafica

--- src/main/java/com/mycompany/lenguajesproyecto/LenguajesProyecto.java
+++ src/main/java/com/mycompany/lenguajesproyecto/LenguajesProyecto.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lenguajesproyecto;
 
+import com.mycompany.lenguajesproyecto.FrontEnd.PromptZal_FrontEnd_Swimng.MenuInicialSegundo;
+
 /**
  *
  * @author ricardocastillo
@@ -11,6 +13,8 @@ package com.mycompany.lenguajesproyecto;
 public class LenguajesProyecto {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MenuInicialSegundo menu= new MenuInicialSegundo();
+        menu.setVisible(true);
+
     }
 }
